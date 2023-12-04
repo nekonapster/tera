88801e1275e71baa8045850914bb027f341e8487
NekoNapster <mdmgrafica@gmail.com>
2023-12-04 19:17:31 +0100

    tour de js para user

---
+++ tourIndex.js
@@ -0,0 +1,118 @@
+const driver = window.driver.js.driver;
+const driverObj = driver({
+	popoverClass: 'theme1',
+	animate: false,
+	overlayColor: "black",
+	// progressText: "Step {{current}} of {{total}}",
+	showButtons: [
+		'next', 'previous',
+		// 'close'
+	],
+	showProgress: false,
+	steps: [
+		{
+			element: "mainTour",
+			popover: {
+				title: "Nueva web!!!",
+				description: "Hemos actualizado nuestra zona de clientes para aprovechar los espacios y hemos rejuvenecido los estilos"
+			}
+		},
+		{
+			element: "#tuto",
+			popover: {
+				title: "Nuevo!!! Boton Tour",
+				description: "Si quieres volver a ver el tour solo tiene que darle a este boton"
+			}
+		},
+		{
+			element: "#facturas",
+			popover: {
+				title: "Nuevo!!! Zona de Facturas",
+				description: "Hemos agrupado 'Mis Facturas' y 'Localizar Facturas' en una unica zona para tener todo en una sola pantalla."
+			}
+		},
+		{
+			element: "#datos",
+			popover: {
+				title: "Nuevo!!! Boton de Datos",
+				description: "Ahora 'Datos personales', 'Mis Contratos' y 'Notificaciones' estan en una misma pantalla."
+			}
+		}, {
+			element: "#manual",
+			popover: {
+				title: "Nuevo!!! Boton Manual",
+				description: "El manual lo tienes en un unico accedo y directo!"
+			}
+		}, {
+			element: "#localizarFacturas",
+			popover: {
+				title: "Nuevo!!!",
+				description: "Añadimos un filtro de busqueda para las facturas antiguas"
+			}
+		}, {
+			element: "#zonaUC",
+			popover: {
+				title: "Zona de ultima conexion",
+				description: "Podras ver tu ultima conexio desde aqui"
+			}
+		}, {
+			element: "#graficaIndex",
+			popover: {
+				title: "Graficas",
+				description: "Realojamos las graficas para que puedas tenerlas al lado de las facturas que quieras comparar"
+			}
+		}, {
+			element: "#informacionFactura",
+			popover: {
+				title: "Informacion Facturas",
+				description: "El antiguo boton de 'Informacion de Facturas' ha sido sustituido por la informacion plasmada directamente en la pagina."
+			}
+		}, {
+			element: "#zonaFacturas",
+			popover: {
+				title: "Facturas",
+				description: "Las seguiras teniendo todas acumuladas y filtradas, ahora nunca perderas la informacion."
+			}
+		},
+	]
+});
+
+
+// play al tour manualmente
+function iniciarTour() {
+	driverObj.drive();
+}
+
+// guardo todas las cookies en 'clave'
+let clave = document.cookie;
+
+// uso funcion 'leerCookie' y le paso 'clave' para separar todas las cookies y que me devuelva el valor segun la clave que le pase.
+function leerCookie(clave) {
+	var valCookie = "";
+	var buscar = clave + "=";
+	if (document.cookie.length > 0) {
+		pos = document.cookie.indexOf(buscar);
+		if (pos != -1) {
+			pos += buscar.length;
+			fin = document.cookie.indexOf(";", pos);
+			if (fin == -1) 
+				fin = document.cookie.length;
+			
+
+			valCookie = set(document.cookie.substring(pos, fin))
+		}
+	}
+	return valCookie;
+}
+
+// uso leerCookie para buscar la cookie que quiero y lo que me devuelve la meto en una variable
+let cookieIndex = leerCookie('index')
+
+
+// comparo los valores para las dos cookies que quiero y denpendiendo de eso lanzo o no el tour
+if (cookieIndex == "") {
+	driverObj.drive();
+}
+
+// las cookies deben ser colocadas luego de todas las funciones sino siempre tendra cookie asignada antes de hacer la comparacion dando falsa comparacion.
+document.cookie = "index=vistoTourIndex; max-age=3600000";
---
+++ tourUser.js
@@ -0,0 +1,102 @@
+const driver = window.driver.js.driver;
+const driverObj = driver({
+	popoverClass: 'theme1',
+	animate: false,
+	overlayColor: "black",
+	// progressText: "Step {{current}} of {{total}}",
+	showButtons: [
+		'next',
+		'previous',
+		// 'close'
+	  ],
+	showProgress: false,
+	steps: [
+		{
+			element: "#zona1",
+			popover: {
+				title: "Zona uno",
+				description: "Hemos dividido en dos zonas el antiguo contenido. Una para 'Datos Personales', 'Cambio de Contraseña' e 'Informes de Errores'",
+			}
+		},
+		{
+			element: "#zona2",
+			popover: {
+				title: "Zona dos",
+				description: "Y el resto para 'Datos Generales' y 'Datps Cups'"
+			}
+		},
+		{
+			element: "#cambioPass",
+			popover: {
+				title: "Cambio de Contraseña",
+				description: "Desde aqui puedes hacer el cambio de tu contraña."
+			}
+		},
+		{
+			element: "#guardarDatos",
+			popover: {
+				title: "Guardar Datos",
+				description: "Los datos a tu alcanze junto con un boton para hacer las modificaciones que desees."
+			}
+		}, 
+        {
+			element: "#notificaciones",
+			popover: {
+				title: "Nueva zona!!!",
+				description: "Hemos traido la zona de 'Ticket' a una unica pagina central. Puedes usar el desplegable para elegir tu contrato."
+			}
+		},
+        {
+			element: "#datosGenerales",
+			popover: {
+				title: "Datos generelales",
+				description: "Ahora esta a la par sobre los datos de los Cups. Todo a tu alcance y de un solo vistazo."
+			}
+		},
+        {
+			element: "#datosCups",
+			popover: {
+				title: "Datos Cups",
+				description: "Los hemos puesto junto con las potencias contratadas y el precio. Todo facil de ver y facil de comparar."
+			}
+		},
+        ]
+});
+
+
+// play al tour manualmente
+function iniciarTour() {
+	driverObj.drive();
+}
+
+// guardo todas las cookies en 'clave'
+let clave = document.cookie;
+
+// uso funcion 'leerCookie' y le paso 'clave' para separar todas las cookies y que me devuelva el valor segun la clave que le pase.
+function leerCookie(clave) {
+	var valCookie = "";
+	var buscar = clave + "=";
+	if (document.cookie.length > 0) {
+		pos = document.cookie.indexOf(buscar);
+		if (pos != -1) {
+			pos += buscar.length;
+			fin = document.cookie.indexOf(";", pos);
+			if (fin == -1) 
+				fin = document.cookie.length;
+			
+			valCookie = set(document.cookie.substring(pos, fin))
+		}
+	}
+	return valCookie;
+}
+
+//uso leerCookie para buscar la cookie que quiero y lo que me devuelve la meto en una variable
+let cookieUser = leerCookie('user')
+
+//comparo los valores para las dos cookies que quiero y denpendiendo de eso lanzo o no el tour
+if (cookieUser == "") {
+	driverObj.drive();
+}
+
+//las cookies deben ser colocadas luego de todas las funciones sino siempre tendra cookie asignada antes de hacer la comparacion dando falsa comparacion.
+document.cookie = "user=vistoTourUser; max-age=3600000";
